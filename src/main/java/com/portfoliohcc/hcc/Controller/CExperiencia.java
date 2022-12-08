207af45812e14a99af5696def033332fa22824d9
BelacdowN <104211742+BelacdowN@users.noreply.github.com>
2022-12-08 15:10:18 -0300

    correccion de ruta experiencia

--- src/main/java/com/portfoliohcc/hcc/Controller/CExperiencia.java
+++ src/main/java/com/portfoliohcc/hcc/Controller/CExperiencia.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("Experiencia")
+@RequestMapping("experiencia")
 @CrossOrigin(origins = "http://localhost:4200")
 public class CExperiencia {
      @Autowired
